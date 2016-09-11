da1dd90fd3db1276428d9dd544ef26a4bb832da4
dmitrymakhnin <dimamakhnin@gmail.com>
2016-09-11 18:56:08 -0400

    ...

--- modules/dynamic/fs.pl
+++ modules/dynamic/fs.pl
@@ -1,15 +1,13 @@
 (sub {
     my $fs = {
+        ##
+        # readFileSync :: string -> string
+        ##
         'readFileSync' => sub {
             my $filename = $_[0];
 
-            # reminder: you should be familiar of anonymous scopes, 
-            # js example: (function(){console.log('hi')})();
             (sub {
-                # reminder: local "temporarily changes the value within scope".           
-                # reminder: $_ is "arguments".
-                # reminder: $/ is "record separator". defaults to 'newline'. we want whole file.
-                # reminder: <> opens every argument in @ARGV and returns a record from it.
+                # no record separator(read everything).
                 local $/ = undef;
                 local @ARGV = $_[0];
                 <>;
