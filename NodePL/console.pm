2421b8a1e09263b645d33e9f3f9e4c1a5b3ecbda
dmitrymakhnin <dimamakhnin@gmail.com>
2016-12-14 16:10:46 -0500

    improved console.pm

--- NodePL/console.pm
+++ NodePL/console.pm
@@ -1,12 +1,17 @@
 package console;
-use Data::Dumper;
-$Data::Dumper::Deparse = 1;
+use List::Util ('reduce');
 
 sub log {
-    if ("$_[0]" eq __PACKAGE__) {
-        shift @_; 
-    }   
-    print shift . "\n";
+    # | make console::log the same as console->log
+    # | by shifting package if arrow is used.
+    shift @_
+        if "$_[0]" eq __PACKAGE__;
+        
+    # | console.log is varaidic; so it always appends
+    # | the arguments before printing them.
+    # | console.log(1, 2, 3) prints "1 2 3"
+    my $text = reduce {$a . ' ' . $b} @_;
+    print $text . "\n";
 }
 1;
 
