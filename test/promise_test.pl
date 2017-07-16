5546fe996f4882aa8a05306deb68878d2683f487
CodeDmitry <dimamakhnin@gmail.com>
2017-07-16 09:10:36 -0400

    Update promise_test.pl

--- test/promise_test.pl
+++ test/promise_test.pl
@@ -27,36 +27,24 @@ my @tests = (
         ok('test1');
     },
     sub {
-        promise->new(sub {
-            #$_[0]->(1);
-            $_[1]->("failed");
-        })->then(sub {
-            console::log $_[0];
-            return $_[0] + 2;
-        })->then(sub {
-            console::log $_[0];
-            return $_[0] + 3;
-        })->then(sub {
-            console::log $_[0];
-            return $_[0];
-        });    
-        ok('test2');
+        eval {
+            promise->new(sub {
+                #$_[0]->(1);
+                $_[1]->("failed");
+            })->then(sub {
+                console::log $_[0];
+                return $_[0] + 2;
+            })->then(sub {
+                console::log $_[0];
+                return $_[0] + 3;
+            })->then(sub {
+                console::log $_[0];
+                return $_[0];
+            });   
+        };
+        ok('test2') if $@;
     }
 );
-
-(sub {
-    push @tests, sub {
-        my $toWrite = "this is a test";
-        my $str = '';
-        my $fake_stdout;
-        open $fake_stdout, '>', \$str
-            or die "cannot open string as a stream.\n";
-        select $fake_stdout;
-        console::log $toWrite, $toWrite, $toWrite;
-        
-        ok($str eq "$toWrite $toWrite $toWrite\n", 'console::log test behaves as expected.');
-    };
-})->();
 
 plan tests => scalar @tests;
 
