b544aecb9f591d272df2eed8d44177830c7d5502
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-26 04:08:58 -0400

    Update fs.pm

--- NodePL/fs.pm
+++ NodePL/fs.pm
@@ -8,16 +8,21 @@ takes care of closing the file.
 =cut
 # | end comment
 sub with {
+    my $name;
+    my $cb;
+    my $file;
+
     # | make fs::with the same as fs->with
     # | by shifting package if arrow is used.
-    shift @_ 
-        if (defined $_[0] and $_[0] eq __PACKAGE__);
+    if (defined $_[0] and $_[0] eq __PACKAGE__) {
+        shift(@_);
+    }
 
-    my $name = $_[0];
-    my $cb = $_[1];
+    $name = $_[0];
+    $cb = $_[1];
     
-    open my $file, '<', $name
-        or die "$!\n";       
+    open($file, '<', $name)
+        or die("$!\n");       
 
     $cb->($file);
 
@@ -31,16 +36,23 @@ php file_get_contents
 =cut
 # | end
 sub get_contents {
+    my $file;
+    my $text;
+    
     # | make fs::get_contents the same as fs->get_contents
     # | by shifting package if arrow is used.
-    shift @_ 
-        if (defined $_[0] and $_[0] eq __PACKAGE__);
+    if (defined $_[0] and $_[0] eq __PACKAGE__) {
+        shift(@_);
+    }
 
     local $/ = undef;
-    open my $file, '<', $_[0]
+    open($file, '<', $_[0])
         or die "$!\n";
-    my $text = <$file>;
+        
+    $text = <$file>;
+
     close $file;
+    
     return $text;
 }
 
@@ -51,15 +63,20 @@ php file_get_contents
 =cut
 # | end
 sub get_contents_async {
+    my $fileName;
+    my $callback;
+    my $data;
+    
     # | make fs::get_contents_async the same 
     # |     as fs->get_contents_sync
     # | by shifting package if arrow is used.
-    shift @_ 
-        if (defined $_[0] and $_[0] eq __PACKAGE__);
+    if (defined $_[0] and $_[0] eq __PACKAGE__) {
+        shift(@_); 
+    }
 
-    my $fileName = $_[0];
-    my $callback = $_[1];
-    my $data = fs::get_contents($fileName);
+    $fileName = $_[0];
+    $callback = $_[1];
+    $data = fs::get_contents($fileName);
     $callback->($data);
 }
 
